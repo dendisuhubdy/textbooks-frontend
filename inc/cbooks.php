f802fd686ebba66453bdcf6bd2d43f4b2d62d9b3
dendisuhubdy <dendi.suhubdy@gmail.com>
2015-10-18 02:09:26 -0400

    merging design 1

---
+++ inc/cbooks.php
@@ -0,0 +1,23 @@
+<?php
+
+class books {
+    public $md5;
+    public $id;
+    public $title;
+    public $edition;
+    public $img;
+    public $authors;
+    public $description
+    public $pages;
+    public $publisher
+    public $language;
+    public $isbn-10;
+    public $isbn-13;
+    public $category;
+    public $keywords;
+
+    function show($id) {
+
+    }
+
+}
